3005370cd35fbf3ee29445266f40f9b29e4b35f1
TRMO <tromortensen@hotmail.com>
2023-03-13 15:59:47 +0100

    add a bit of java doc

--- Server/Core/Application/src/main/java/com/pastimegames/readysetbet/core/application/gamemanager/GameManager.java
+++ Server/Core/Application/src/main/java/com/pastimegames/readysetbet/core/application/gamemanager/GameManager.java
@@ -5,14 +5,29 @@ import com.pastimegames.readysetbet.shared.viewmodels.PlayerVM;
 
 public interface GameManager {
     void joinPlayer(PlayerVM playerdto);
+
+    /**
+     * Use when lobby stuff is done, and you want to start the races
+     * @param options
+     */
     void prepareForRacing(RaceOptions options);
 
+    /**
+     * Start a race
+     */
     void startRace();
 
     void removePlayerFromLobby(String playerName);
 
+
+    /**
+     * Use when a race is finished, and you want an overview of the results.
+     */
     void displayResults();
 
+    /**
+     * After viewing the race results, use this method to start the next race
+     */
     void nextRace();
 }
 
