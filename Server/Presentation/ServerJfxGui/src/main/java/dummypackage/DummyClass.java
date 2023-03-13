297ee3b41bae8a428968e6625f12224189c657f1
MIVI <mivi@via.dk>
2023-03-13 11:41:13 +0100

    skelet for servergui

--- Server/Presentation/ServerJfxGui/src/main/java/dummypackage/DummyClass.java
+++ Server/Presentation/ServerJfxGui/src/main/java/dummypackage/DummyClass.java
@@ -1,4 +1,34 @@
 package dummypackage;
 
-public class DummyClass {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DummyClass extends Application
+{
+  @Override public void start(Stage stage) throws IOException
+  {
+    FXMLLoader fxmlLoader = new FXMLLoader(
+        DummyClass.class.getResource("ServerMainView.fxml"));
+    Scene scene = null;
+    try
+    {
+      scene = new Scene(fxmlLoader.load(), 320, 240);
+    }
+    catch (IOException e)
+    {
+      throw new RuntimeException(e);
+    }
+    stage.setTitle("Hello!");
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public static void main(String[] args)
+  {
+    launch();
+  }
 }
---
+++ Server/Presentation/ServerJfxGui/src/main/java/dummypackage/ServerMainViewController.java
@@ -0,0 +1,12 @@
+package dummypackage;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ServerMainViewController
+{
+
+
+
+}
