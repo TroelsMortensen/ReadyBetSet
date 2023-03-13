99725c75983529ab050a5034406b75ec8c17d581
TRMO <tromortensen@hotmail.com>
2023-03-13 15:22:46 +0100

    add more race options

--- Server/Core/Domain/src/main/java/com/pastimegames/readysetbet/core/domain/valueobjects/RaceOptions.java
+++ Server/Core/Domain/src/main/java/com/pastimegames/readysetbet/core/domain/valueobjects/RaceOptions.java
@@ -3,13 +3,33 @@ package com.pastimegames.readysetbet.core.domain.valueobjects;
 public class RaceOptions {
 
     private int moveTickTime = 1000;
+    private int startCoins = 0;
+    private int numberOfAllowedBidsPerCell = 1;
 
     public RaceOptions setMoveTickTimeInMs(int moveTickTime) {
         this.moveTickTime = moveTickTime;
         return this;
     }
 
+    public RaceOptions setStartCoins(int startCoins) {
+        this.startCoins = startCoins;
+        return this;
+    }
+
+    public RaceOptions setNumberOfAllowedBidsPerCell(int numberOfAllowedBidsPerCell) {
+        this.numberOfAllowedBidsPerCell = numberOfAllowedBidsPerCell;
+        return this;
+    }
+
     public int moveTickTime(){
         return moveTickTime;
+    }
+
+    public int startCoins(){
+        return startCoins;
+    }
+
+    public int numberOfAllowedBidsPerCell() {
+        return numberOfAllowedBidsPerCell;
     }
 }
