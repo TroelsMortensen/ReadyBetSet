d5dd39608b2fb5da4bc9afce2a9d48aa07751001
Troels Mortensen <tromortensen@hotmail.com>
2023-03-13 11:14:58 +0100

    renamed socket server, updated poms. Made DTO module

--- Server/Core/Application/src/main/java/com/pastimegames/readysetbet/core/application/gamemanager/GameManager.java
+++ Server/Core/Application/src/main/java/com/pastimegames/readysetbet/core/application/gamemanager/GameManager.java
@@ -1,4 +1,17 @@
 package com.pastimegames.readysetbet.core.application.gamemanager;
 
+import com.pastimegames.readysetbet.core.domain.entities.lobby.Lobby;
+import com.pastimegames.shared.datatransferobjects.PlayerDto;
+
 public class GameManager {
+
+    private Lobby lobby;
+
+    public GameManager() {
+        lobby = new Lobby();
+    }
+
+    public void joinPlayer(PlayerDto player){
+
+    }
 }
---
+++ Shared/DataTransferObjects/src/main/java/com/pastimegames/shared/datatransferobjects/PlayerDto.java
@@ -0,0 +1,4 @@
+package com.pastimegames.shared.datatransferobjects;
+
+public record PlayerDto(String playerName, String colorCode) {
+}
