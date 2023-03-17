6e4fad00f6869e090f74766ec16cc91b29ba435c
Troels Mortensen <tromortensen@hotmail.com>
2023-03-17 14:08:10 +0100

    setup structure for bet board cells to be filled out

--- Server/Core/Domain/src/main/java/com/pastimegames/readysetbet/core/domain/entities/betting/BettingBoard.java
+++ Server/Core/Domain/src/main/java/com/pastimegames/readysetbet/core/domain/entities/betting/BettingBoard.java
@@ -1,16 +1,24 @@
 package com.pastimegames.readysetbet.core.domain.entities.betting;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class BettingBoard {
 
     private List<BetCell> betCells = new ArrayList<>();
     private BetCell[] cells = new BetCell[]{
-            new BetCell(4, -4), new BetCell(4, -3), new BetCell(5, -4), new BetCell(5, -3), new BetCell(7, -2), new BetCell(8, -2), new BetCell(9, -2),
-// TODO make the rest of the rows here
+            /* 1 */ new BetCell(4, -4), new BetCell(4, -3), new BetCell(5, -4), new BetCell(5, -3), new BetCell(7, -2), new BetCell(8, -2), new BetCell(9, -2),
+            /* 2 */ new BetCell(3, -1),
+            /* 3 */ new BetCell(2, -3),
+            /* 4 */ new BetCell(1, -2),
+            /* 5 */ new BetCell(0, 0),
+            /* 6 */ new BetCell(1, -2),
+            /* 7 */ new BetCell(2, -3),
+            /* 8 */ new BetCell(3, -1),
+            /* 9 */ new BetCell(4, -4), new BetCell(4, -3), new BetCell(5, -4), new BetCell(5, -3), new BetCell(7, -2), new BetCell(8, -2), new BetCell(9, -2)
     };
+
+// TODO make the rest of the rows here
 
     public BettingBoard() {
         for (int i = 0; i < cells.length; i++) {
