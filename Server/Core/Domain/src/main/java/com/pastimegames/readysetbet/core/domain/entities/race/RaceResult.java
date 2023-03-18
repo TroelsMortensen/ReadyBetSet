bd6e5ac881bf55ddb36c43c45b46465d1a229de7
TRMO <tromortensen@hotmail.com>
2023-03-18 14:39:40 +0100

    refactored the win, place, show calculations to do it on creation, instead of for each request. Optimization.

--- Server/Core/Domain/src/main/java/com/pastimegames/readysetbet/core/domain/entities/race/RaceResult.java
+++ Server/Core/Domain/src/main/java/com/pastimegames/readysetbet/core/domain/entities/race/RaceResult.java
@@ -4,35 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public record RaceResult(Map<String, HorseData> horses) {
-    public record HorseData(String name, HorseColor color, int finalPosition) {
+public class RaceResult {
+    public record HorseData(String name, HorseColor color, int finalPosition) {}
 
+    private final Map<String, HorseData> horses;
+
+    private final int place;
+    private final int show;
+
+    public RaceResult(Map<String, HorseData> horses) {
+        this.horses = horses;
+        List<HorseData> list = new ArrayList<>(horses.values().stream().toList());
+        list.sort((o1, o2) -> o2.finalPosition - o1.finalPosition);
+        if (list.get(0).finalPosition != 15) {
+            throw new RuntimeException("Failure");
+        }
+        place = list.get(1).finalPosition;
+        list.removeIf(horseData -> horseData.finalPosition >= place);
+        show = list.get(0).finalPosition;
     }
-    // TODO I need unit tests for these calculations
+
+    public Map<String, HorseData> horses() {
+        return horses;
+    }
+
     public boolean isWIN(String horseName) {
         return horses.get(horseName).finalPosition == 15;
     }
 
     public boolean isPLACE(String horseName) {
-        return horses.get(horseName).finalPosition >= findSecondPlaceIndex();
+        return horses.get(horseName).finalPosition >= place;
     }
 
     public boolean isSHOW(String horseName) {
-        return horses.get(horseName).finalPosition >= findThirdPlaceIndex();
+        return horses.get(horseName).finalPosition >= show;
     }
-
-    private int findSecondPlaceIndex() {
-        List<HorseData> list = new ArrayList<>(horses.values().stream().toList());
-        list.sort((o1, o2) -> Integer.compare(o2.finalPosition, o1.finalPosition));
-        return list.get(1).finalPosition;
-    }
-
-    private int findThirdPlaceIndex() {
-        List<HorseData> list = new ArrayList<>(horses.values().stream().toList());
-        list.removeIf(horseData -> horseData.finalPosition == 15);
-        list.removeIf(horseData -> horseData.finalPosition == findSecondPlaceIndex());
-        list.sort((o1, o2) -> o2.finalPosition - o1.finalPosition);
-        return list.get(0).finalPosition;
-    }
-
 }
