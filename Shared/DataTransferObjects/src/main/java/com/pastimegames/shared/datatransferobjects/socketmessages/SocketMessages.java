1fb864e39c2289c08975236ad1ce0c78c4e3eb22
Troels Mortensen <tromortensen@hotmail.com>
2023-03-22 10:27:42 +0100

    making sub-classes static

--- Shared/DataTransferObjects/src/main/java/com/pastimegames/shared/datatransferobjects/socketmessages/SocketMessages.java
+++ Shared/DataTransferObjects/src/main/java/com/pastimegames/shared/datatransferobjects/socketmessages/SocketMessages.java
@@ -3,23 +3,23 @@ package com.pastimegames.shared.datatransferobjects.socketmessages;
 public class SocketMessages
 {
 
-  public class Events
+  public static class Events
   {
     public static final String ERROR = "error";
 
-    public class Race
+    public static class Race
     {
       public static final String HORSE_MOVED = "horse_moved";
       public static final String RACE_FINISHED = "horse_moved";
     }
 
-    public class Betting
+    public static class Betting
     {
       public static final String BET_PLACED = "bet_placed";
       public static final String BETS_ARE_CLOSED = "bets_are_closed";
     }
 
-    public class Lobby
+    public static class Lobby
     {
       public static final String PLAYER_JOINED = "player_joined";
       public static final String PLAYER_LEFT = "player_left";
@@ -28,10 +28,10 @@ public class SocketMessages
 
   }
 
-  public class Commands
+  public static class Commands
   {
 
-    public class Lobby
+    public static class Lobby
     {
       public static final String JOIN = "lobby/join";
       public static final String LEAVE = "lobby/leave";
